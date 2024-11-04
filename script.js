e0d97c119b066b4a1ea1d8c72d60b05640c1629a
dabrwa <dabrwa1000@gmail.com>
2024-11-04 16:25:03 +0000

    computer & human selection, scores and one round function complete

---
+++ script.js
@@ -0,0 +1,77 @@
+
+
+//COMPUTER CHOICE 
+//The function picks a random number between 1-3 and gives each number a value of Rock, Paper or Scissors
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random()* 3) + 1;
+    
+    if (randomNumber == 1) {
+        return "rock";
+    } else if (randomNumber == 2) {
+        return "paper";
+    } else {
+    return "scissors";
+    }
+}
+
+let computerSelection = getComputerChoice();
+computerSelection = computerSelection.toLowerCase();
+console.log("I choose " + computerSelection);
+
+
+//HUMAN CHOICE
+//This function asks the player to pick Rock, Paper or Scissors and returns what they have picked
+function getHumanChoice() {
+    let humanChoice = prompt("Please make your choice from Rock, Paper or Scissors");
+    return humanChoice;
+}
+
+let humanSelection = getHumanChoice();
+humanSelection = humanSelection.toLowerCase();
+console.log(humanSelection);
+
+
+
+//SCORES
+let computerScore = 0;
+let humanScore = 0;
+
+
+//PLAY ROUND
+// a function that takes the human and computer player choices as arguments, 
+//plays a single round, 
+//increments the round winner’s score and logs a winner announcement. */
+
+function playRound(computerChoice, humanChoice) {
+
+    if (computerChoice === "rock" && humanChoice === "paper") {
+        humanScore++
+        return "Well done! you win, Paper beats Rock";
+        
+    } else if (computerChoice === "paper" && humanChoice === "rock") {
+        computerScore++
+        return "You Lose! Paper beats Rock"; 
+
+    } else if (computerChoice === "scissors" && humanChoice === "rock") {
+        humanScore++
+        return "Well done! you win, Rock beats Scissors"; 
+
+    } else if (computerChoice === "rock" && humanChoice === "scissors") {
+        computerScore++
+        return "You lose! Rock beats Scissors"; 
+    
+    } else if (computerChoice === "paper" && humanChoice === "scissors") {
+        humanScore++
+        return "Well done! you win, Scissors beats Paper"; 
+    
+    } else if (computerChoice === "scissors" && humanChoice === "paper") {
+        computerScore++
+        return "You lose! Scissors beats Paper"; 
+    
+    } else  {
+        return "its a draw";
+    }
+}
+
+console.log(playRound(computerSelection, humanSelection)); 
+console.log("The score is - " + "Computer: " + computerScore + " You: " + humanScore);
